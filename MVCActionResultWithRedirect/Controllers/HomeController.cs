15fe3983240e036242e5a9c40e9596ff639905a2
Jose Ortega <jortega928@yahoo.com>
2018-10-26 18:17:25 -0500

    Another answer 26102018

---
+++ MVCActionResultWithRedirect/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCActionResultWithRedirect.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+      
+
+        public ActionResult Upload()
+        {
+       
+            return new RedirectResult("http://www.google.com");
+       
+        }
+    }
+}
---
+++ MVCActionResultWithRedirect/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCActionResultWithRedirect.MvcApplication" Language="C#" %>
