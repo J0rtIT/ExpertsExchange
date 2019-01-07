d94bd177966abccc54682655c2be42abb38e8081
Jose Ortega <jortega928@yahoo.com>
2019-01-06 20:15:11 -0600

    NewAnswerAdd

---
+++ MVCTableWhenDataisEmpty/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using MVCTableWhenDataisEmpty.Models;
+using System.Web.Mvc;
+
+namespace MVCTableWhenDataisEmpty.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Table()
+        {
+            return View(new DateClass());
+        }
+
+    }
+}
---
+++ MVCTableWhenDataisEmpty/Models/DateClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MVCTableWhenDataisEmpty.Models
+{
+    public class DateClass
+    {
+        public DateTime EmptyDate { get; set; }
+        public DateTime TodayDate { get; set; }
+
+        public DateClass()
+        {
+            EmptyDate = new DateTime();
+            TodayDate = DateTime.Now;
+        }
+
+        public bool isItEmpty(DateTime dt)
+        {
+            DateTime tester = new DateTime(1970, 01, 01, 0, 0, 0, 0);
+
+            if (dt > tester)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
