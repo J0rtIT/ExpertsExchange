b6f3b18439986e3e28a326c6c7fe301e7c1c4354
Jose Ortega <jortega928@yahoo.com>
2019-01-30 11:27:36 -0600

    Formating a string line with several spaces using Split

---
+++ FormatingText/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace FormatingText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(@"D:\ghcode\j0rt3g4\ExpertsExchange\FormatingText\backup_files.log.txt");
+
+            string date;
+            string time;
+            string error;
+            //var sb = new StringBuilder();
+            foreach (var line in lines)
+            {
+
+                if (line.StartsWith("W"))
+                {
+                    String[] test = line.Split(new char[] { ' ' }, 6);
+                    date = test[1];
+                    time = test[2];
+                    error = test[5];
+
+                    //sb.AppendLine(line);
+                }
+            }
+
+
+        }
+    }
+}
